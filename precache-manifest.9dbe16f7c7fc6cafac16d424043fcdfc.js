75f58fb86ce5f205eb4f61f7502405ca26a1feb8
Oliver <oliverahmed000@gmail.com>
2022-06-14 21:23:16 +0600

    Updates

---
+++ precache-manifest.9dbe16f7c7fc6cafac16d424043fcdfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51287f3117563df4fe06b0657d91cea4",
+    "url": "/freeCodeCamp_react-projects/14-Cart/index.html"
+  },
+  {
+    "revision": "328383caf74d3d805d26",
+    "url": "/freeCodeCamp_react-projects/14-Cart/static/css/main.8db4b5e8.chunk.css"
+  },
+  {
+    "revision": "22eaee84a4c11238e3f9",
+    "url": "/freeCodeCamp_react-projects/14-Cart/static/js/2.33b3b3b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/freeCodeCamp_react-projects/14-Cart/static/js/2.33b3b3b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328383caf74d3d805d26",
+    "url": "/freeCodeCamp_react-projects/14-Cart/static/js/main.15eb7abd.chunk.js"
+  },
+  {
+    "revision": "2a59d87cc2ac361c090d",
+    "url": "/freeCodeCamp_react-projects/14-Cart/static/js/runtime-main.daaeb2fa.js"
+  }
+]);
